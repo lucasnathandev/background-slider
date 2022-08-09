d09877b134f4c5cfed8ba7839fa0263a6e9c608f
Solium Dev <lucas.develop.prog@gmail.com>
2022-08-08 22:10:47 -0300

    :sparkles: :tada: Project finished and ready for deploy.

---
+++ script.js
@@ -0,0 +1,54 @@
+const body = document.body
+const slides = document.querySelectorAll(".slide")
+const leftBtn = document.querySelector("#left")
+const rightBtn = document.querySelector("#right")
+
+let activeSlide = 0
+
+function setBgToBody(index) {
+  body.style.backgroundImage = slides[index].style.backgroundImage
+}
+
+function setBgToSlides(imgs) {
+  slides.forEach((slide, i) => {
+    slide.style.backgroundImage = `url(${imgs[i]})`
+  })
+}
+
+function setActiveSlide(index) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active")
+  })
+  slides[index].classList.add("active")
+}
+
+function nextImg() {
+  if (!(activeSlide + 1 < slides.length && activeSlide + 1 >= 0)) return
+  activeSlide++
+  setActiveSlide(activeSlide)
+  setBgToBody(activeSlide)
+}
+
+function prevImg() {
+  if (!(activeSlide - 1 < slides.length && activeSlide - 1 >= 0)) return
+  activeSlide--
+  setActiveSlide(activeSlide)
+  setBgToBody(activeSlide)
+}
+
+async function getImages(count) {
+  const data = await fetch(
+    `https://api.unsplash.com/search/photos?query=nature&count=${count}&client_id=FRqKIeiMragyXqayDSOrx2_TkUkuze2HZD13ElztWu8`
+  )
+  return (await data.json()).results.map((img) => img.urls.full)
+}
+
+async function main() {
+  const imgs = await getImages(slides.length)
+  setBgToSlides(imgs)
+  setBgToBody(activeSlide)
+  leftBtn.addEventListener("pointerup", prevImg)
+  rightBtn.addEventListener("pointerup", nextImg)
+}
+
+main()
